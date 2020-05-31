c0bf3c2c353d13a2bc00aa9081056011c9d736a8
corvofeng <corvofeng@gmail.com>
2020-05-31 10:19:03 +0800

    Update index handler and add log output.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- controller/article.go
+++ controller/article.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"sort"
 	"strconv"
@@ -270,74 +269,6 @@ func (h *BaseHandler) ArticleAddPost(w http.ResponseWriter, r *http.Request) {
 		aobj.ID,
 	}
 	json.NewEncoder(w).Encode(tmp)
-}
-
-// ArticleHomeList 文章主页
-func (h *BaseHandler) ArticleHomeList(w http.ResponseWriter, r *http.Request) {
-	btn, key, score := r.FormValue("btn"), r.FormValue("key"), r.FormValue("score")
-	var start uint64
-	var err error
-
-	if len(key) > 0 {
-		start, err = strconv.ParseUint(key, 10, 64)
-		if err != nil {
-			w.Write([]byte(`{"retcode":400,"retmsg":"key type err"}`))
-			return
-		}
-	}
-	if len(score) > 0 {
-		_, err = strconv.ParseUint(score, 10, 64)
-		if err != nil {
-			w.Write([]byte(`{"retcode":400,"retmsg":"score type err"}`))
-			return
-		}
-	}
-
-	scf := h.App.Cf.Site
-	redisDB := h.App.RedisDB
-
-	type pageData struct {
-		PageData
-		SiteInfo model.SiteInfo
-		PageInfo model.ArticlePageInfo
-		Links    []model.Link
-	}
-
-	var count uint64
-	sqlDB := h.App.MySQLdb
-	// 获取全部的帖子数目
-	err = sqlDB.QueryRow("SELECT COUNT(*) FROM topic").Scan(&count)
-	if err != nil {
-		log.Printf("Error %s", err)
-		return
-	}
-
-	// 获取贴子列表
-	pageInfo := model.SQLArticleList(sqlDB, redisDB, start, btn, uint64(scf.HomeShowNum), scf.TimeZone)
-	categories, err := model.SQLGetAllCategory(sqlDB)
-
-	tpl := h.CurrentTpl(r)
-	evn := &pageData{}
-	evn.SiteCf = scf
-	evn.Title = scf.Name
-	evn.Keywords = evn.Title
-	evn.Description = scf.Desc
-	evn.IsMobile = tpl == "mobile"
-	currentUser, _ := h.CurrentUser(w, r)
-	evn.CurrentUser = currentUser
-	evn.ShowSideAd = false
-	evn.PageName = "home"
-	evn.NewestNodes = categories
-	// evn.HotNodes = model.CategoryHot(db, scf.CategoryShowNum)
-	// evn.NewestNodes = model.CategoryNewest(db, scf.CategoryShowNum)
-
-	evn.SiteInfo = model.GetSiteInfo(redisDB)
-	evn.PageInfo = pageInfo
-
-	// 右侧的链接
-	evn.Links = model.RedisLinkList(redisDB, false)
-
-	h.Render(w, tpl, evn, "layout.html", "index.html")
 }
 
 // IFeelLucky 随机的抽取一些帖子
