eb22d0daf2fd8d02ab495c7633ff54dedfc4080b
corvofeng <corvofeng@gmail.com>
2020-06-03 22:06:19 +0800

    Start add webpack for extensions.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const config = require('flarum-webpack-config');
+const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path');
+const merge = require('webpack-merge');
+
+module.exports = {
+  entry: function () {
+    const entries = {};
+
+    for (const app of ['forum', 'admin']) {
+      const file = path.resolve(process.cwd(), "view", "flarum", app + '.js');
+      if (fs.existsSync(file)) {
+        entries[app] = file;
+      }
+    }
+
+    return entries;
+  }(),
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { modules: false, loose: true }],
+              ['@babel/preset-react']
+            ],
+            plugins: [
+              ['@babel/plugin-transform-runtime', { useESModules: true }],
+              ['@babel/plugin-proposal-class-properties'],
+              ['@babel/plugin-transform-react-jsx', { pragma: 'm' }],
+              ['@babel/plugin-transform-object-assign']
+            ]
+          }
+        }
+      }
+    ]
+  },
+
+  /*
+  output: {
+    path: path.resolve(process.cwd(), 'dist'),
+    library: 'module.exports',
+    libraryTarget: 'assign',
+    devtoolNamespace: require(path.resolve(process.cwd(), 'package.json')).name
+  },
+  */
+ 
+
+  externals: [
+    {
+      '@flarum/core/forum': 'flarum.core',
+      '@flarum/core/admin': 'flarum.core',
+      'jquery': 'jQuery',
+    },
+
+    function () {
+      const externals = {};
+
+      if (options.useExtensions) {
+        for (const extension of options.useExtensions) {
+          externals['@' + extension] =
+            externals['@' + extension + '/forum'] =
+            externals['@' + extension + '/admin'] = "flarum.extensions['" + extension + "']";
+        }
+      }
+
+      return externals;
+    }(),
+
+    // Support importing old-style core modules.
+    function (context, request, callback) {
+      let matches;
+      if ((matches = /^flarum\/(.+)$/.exec(request))) {
+        return callback(null, 'root flarum.core.compat[\'' + matches[1] + '\']');
+      }
+      callback();
+    }
+  ],
+
+  devtool: 'source-map',
+
+  output: {
+    library: 'flarum.core'
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+  ]
+}
