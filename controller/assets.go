8225f5dc1f0fa78d15130fbe989c66b2ee282328
corvofeng <corvofeng@gmail.com>
2020-06-03 09:29:13 +0800

    Add locale dir, add logger for locale.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- controller/assets.go
+++ controller/assets.go
@@ -10,11 +10,10 @@ import (
 
 // GetLocaleData 获取地区对应的语言包
 func (h *BaseHandler) GetLocaleData(w http.ResponseWriter, r *http.Request) {
-	localeDir := path.Join(h.App.Cf.Main.ViewDir, "..", "locale")
+	localeDir := path.Join(h.App.Cf.Main.LocaleDir)
 	localeDataArr := util.FlarumReadLocale(localeDir, "en")
 
 	arr, _ := json.Marshal(localeDataArr)
-
 	retData := fmt.Sprintf(`flarum.core.app.translator.addTranslations(%s);`, arr)
 	w.Header().Set("Content-Type", "text/javascript; charset=utf-8")
 	w.Header().Set("Cache-Control", "public, max-age=31536000")
