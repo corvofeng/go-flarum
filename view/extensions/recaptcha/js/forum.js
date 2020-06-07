ea47db93d37019a67a8956cacd26b61b683b5887
corvofeng <corvofeng@gmail.com>
2020-06-07 20:01:56 +0800

    Add captcha as a extension.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- view/extensions/recaptcha/js/forum.js
+++ view/extensions/recaptcha/js/forum.js
@@ -1,18 +1,24 @@
-import { extend } from 'flarum/extend';
+import { extend, override } from 'flarum/extend';
 import app from 'flarum/app';
+import Component from 'flarum/Component';
 import LogInButtons from 'flarum/components/LogInButtons';
 import LogInButton from 'flarum/components/LogInButton';
+import SignUpModal from 'flarum/components/SignUpModal';
+import ModalManager from 'flarum/components/ModalManager';
+import LoadingIndicator from 'flarum/components/LoadingIndicator';
+
+export * from './src/forum';
 
 
-app.initializers.add('flarum-auth-github', () => {
-  extend(LogInButtons.prototype, 'items', function(items) {
-    items.add('github',
-      <LogInButton
-        className="Button LogInButton--github"
-        icon="fab fa-github"
-        path="/auth/github">
-        {app.translator.trans('flarum-auth-github.forum.log_in.with_github_button')}
-      </LogInButton>
-    );
-  });
-});
+// app.initializers.add('flarum-auth-github', () => {
+//   extend(LogInButtons.prototype, 'items', function(items) {
+//     items.add('github',
+//       <LogInButton
+//         className="Button LogInButton--github"
+//         icon="fab fa-github"
+//         path="/auth/github">
+//         {app.translator.trans('flarum-auth-github.forum.log_in.with_github_button')}
+//       </LogInButton>
+//     );
+//   });
+// });
