7108b5ef5c151ff87e7d2c10e38d1e4e5d4469e2
corvofeng <corvofeng@gmail.com>
2020-06-18 22:07:36 +0800

    Fix mention extension.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- view/extensions/forum.js
+++ view/extensions/forum.js
@@ -1,6 +1,8 @@
 
 export * from './emoji/js/forum';
 export * from './recaptcha/js/forum';
-export * from './mentions/js/forum';
+import * as flarum_mentions from './mentions/js/forum';
 export * from './tags/js/forum';
 export * from './markdown/js/forum';
+
+flarum.extensions["flarum-mentions"] = flarum_mentions;
