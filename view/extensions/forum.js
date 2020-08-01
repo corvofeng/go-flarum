ffdaf060e8c37b9349e02fe9841711f77de74a99
corvofeng <corvofeng@gmail.com>
2020-08-01 16:30:11 +0800

    Add new extensions.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- view/extensions/forum.js
+++ view/extensions/forum.js
@@ -4,5 +4,7 @@ export * from './recaptcha/js/forum';
 import * as flarum_mentions from './mentions/js/forum';
 export * from './tags/js/forum';
 export * from './markdown/js/forum';
+export * from './diff/js/forum';
+export * from './likes/js/forum';
 
 flarum.extensions["flarum-mentions"] = flarum_mentions;
