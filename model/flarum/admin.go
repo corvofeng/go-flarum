f71e0c9d044bf02c677a2d9ea32d1e42bd57ea89
corvofeng <corvofeng@gmail.com>
2021-08-24 09:44:15 +0800

    Add support for plugin icon display.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- model/flarum/admin.go
+++ model/flarum/admin.go
@@ -1,11 +1,14 @@
 package flarum
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"path"
+	"path/filepath"
 	"strings"
 	"zoe/util"
 )
@@ -63,24 +66,82 @@ type FlarumAdminPermissions struct {
 // 作为json文件读取进来就可以了
 type FlarumAdminExtension interface{}
 
-//       "dist": {
-//         "type": "zip",
-//         "url": "https://api.github.com/repos/flarum/emoji/zipball/f2eb25e9383dd05f9905d2ea087f2e997aae2bb0",
-//         "reference": "f2eb25e9383dd05f9905d2ea087f2e997aae2bb0",
-//         "shasum": ""
-//       },
-func ReadExtensionMetadata(extionsDir string) (map[string]FlarumAdminExtension, error) {
-	getIcon := func() map[string]string {
-		return map[string]string{
-			"name":            "fas fa-flag",
-			"backgroundColor": "#D659B5",
-			"color":           "#fff",
-		}
+func extGetLink(extData PluginComposer) map[string]interface{} {
+	links := make(map[string]interface{})
+	for k, v := range extData.Support {
+		links[k] = v
 	}
+	authors := []string{}
+	for _, aObj := range extData.Authors {
+		authors = append(authors, aObj.Name)
+	}
+	links["authors"] = authors
+	return links
+}
 
+type FlarumExtIcon struct {
+	Name            string `json:"name"`
+	BackgroundColor string `json:"backgroundColor"`
+	Color           string `json:"color"`
+	Image           string `json:"image"`
+	BackgroundImage string `json:"backgroundImage"`
+
+	BackgroundSize     string `json:"backgroundSize"`
+	BackgroundRepeat   string `json:"backgroundRepeat"`
+	BackgroundPosition string `json:"backgroundPosition"`
+}
+
+type PluginComposer struct {
+	// 必须额外加入
+	ID    string                 `json:"id"`
+	Path  string                 `json:"path"`
+	Links map[string]interface{} `json:"links"`
+	Icon  FlarumExtIcon          `json:"icon"`
+
+	InstallPath        string                 `json:"install-path"`
+	InstallationSource string                 `json:"install-source"`
+	HasMigrations      bool                   `json:"hasMigrations"`
+	HasAssets          bool                   `json:"hasAssets"`
+	Source             map[string]interface{} `json:"source"`
+
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Keywords    []string `json:"keywords"`
+	Type        string   `json:"type"`
+	License     string   `json:"license"`
+	Authors     []struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Homepage string `json:"homepage,omitempty"`
+	} `json:"authors"`
+	Support map[string]string `json:"support"`
+	Require map[string]string `json:"require"`
+
+	Extra struct {
+		FlarumExtension struct {
+			Title string        `json:"title"`
+			Icon  FlarumExtIcon `json:"icon"`
+		} `json:"flarum-extension"`
+		Flagrow struct {
+			Discuss string `json:"discuss"`
+		} `json:"flagrow"`
+	} `json:"extra"`
+	Autoload interface{} `json:"autoload"`
+	Replace  interface{} `json:"replace"`
+}
+
+func toBase64(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func ReadExtensionMetadata(extionsDir string) (map[string]FlarumAdminExtension, error) {
 	exts := make(map[string]FlarumAdminExtension)
-	items, err := ioutil.ReadDir(extionsDir)
-	if util.CheckError(err, "读取配置") {
+	absPath, err := filepath.Abs(extionsDir)
+	if util.CheckError(err, "查找扩展绝对路径") {
+		return exts, errors.New("无法找到扩展目录")
+	}
+	items, err := ioutil.ReadDir(absPath)
+	if util.CheckError(err, "读取扩展目录") {
 		return exts, errors.New("无法找到扩展目录")
 	}
 	for _, item := range items {
@@ -91,37 +152,54 @@ func ReadExtensionMetadata(extionsDir string) (map[string]FlarumAdminExtension, 
 				// 某个扩展读取信息失败, 仅打印日志, 不做处理
 				continue
 			}
-			var extData map[string]interface{}
+			// var extData map[string]interface{}
+			var extData PluginComposer
 			_ = json.Unmarshal([]byte(data), &extData)
+
 			// 由于admin页面类似锚点路由, 因此这里需要把/转换一次
-			extName := strings.ReplaceAll(extData["name"].(string), "/", "-")
-			extData["id"] = extName
-			extData["links"] = map[string]interface{}{
-				"source":  "https://github.com/flarum/emoji.git",
-				"discuss": "https://discuss.flarum.org",
-				"website": "https://flarum.org",
-				"donate":  "https://flarum.org/donate/",
-				"authors": []string{}, // 必须给定authors
+			extName := strings.ReplaceAll(extData.Name, "/", "-")
+			extData.ID = extName
+
+			extData.Links = extGetLink(extData)
+			extData.Path = path.Join(absPath, item.Name()) // 必须增加path变量
+			extData.Icon = extData.Extra.FlarumExtension.Icon
+
+			if extData.Icon.Image != "" {
+				// 参考flarum/src/Extension/Extension.php, 读取文件作为图标
+				bytes, err := ioutil.ReadFile(path.Join(extData.Path, extData.Icon.Image))
+				if err != nil {
+					log.Fatal(err)
+				}
+				if util.CheckError(err, "读取扩展的图标") {
+					continue
+				}
+				LOGO_MIMETYPES := map[string]string{
+					".svg":  "image/svg+xml",
+					".png":  "image/png",
+					".jpeg": "image/jpeg",
+					".jpg":  "image/jpeg",
+				}
+				mimeType := LOGO_MIMETYPES[filepath.Ext(extData.Icon.Image)]
+				base64Encoding := toBase64(bytes)
+				extData.Icon.BackgroundImage = fmt.Sprintf("url('data:%s;base64,%s')", mimeType, base64Encoding)
 			}
-			extData["path"] = "/flarum/app/vendor/composer/../flarum/likes" // 必须增加path变量
-			extData["icon"] = getIcon()
 
 			// 下面可加可不加
-			extData["install-path"] = "../flarum/flags"
-			extData["type"] = "flarum-extension"
-			extData["installation-source"] = "dist"
-			extData["hasAssets"] = false
-			extData["hasMigrations"] = true
-			extData["source"] = map[string]interface{}{
-				"type":      "git",
-				"url":       "https://github.com/flarum/emoji.git",
-				"reference": "f2eb25e9383dd05f9905d2ea087f2e997aae2bb0",
+			extData.InstallPath = path.Join(absPath, item.Name()) // 必须增加path变量
+			extData.InstallationSource = "dist"
+			extData.HasMigrations = true
+			extData.HasAssets = false
+
+			extData.Source = map[string]interface{}{
+				// "type":      "git",
+				// "url":       "https://github.com/flarum/emoji.git",
+				// "reference": "f2eb25e9383dd05f9905d2ea087f2e997aae2bb0",
 			}
-			extData["time"] = "2021-05-25T20:45:40+00:00"
-			extData["version_normalized"] = "1.0.0.0"
-			extData["version"] = "v1.0.0"
-			extData["extensionDependencyIds"] = []string{}
-			extData["optionalDependencyIds"] = []string{}
+			// extData["time"] = "2021-05-25T20:45:40+00:00"
+			// extData["version_normalized"] = "1.0.0.0"
+			// extData["version"] = "v1.0.0"
+			// extData["extensionDependencyIds"] = []string{}
+			// extData["optionalDependencyIds"] = []string{}
 			exts[extName] = extData
 		}
 	}
