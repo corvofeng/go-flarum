c55812a3f1896aa97d6d182b62a464cd68f5b230
corvofeng <corvofeng@gmail.com>
2022-07-05 11:31:12 +0800

    Add new extension for custom footer.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- view/extensions/admin.js
+++ view/extensions/admin.js
@@ -2,6 +2,7 @@
 export * from './tags/js/admin';
 import * as flarum_mentions from './mentions/js/admin';
 export * from './likes/js/admin';
+export * from './custom-footer/js/admin';
 // export * from './auth-github/js/admin';
 // export * from './analytics/js/admin';
 flarum.extensions["flarum-mentions"] = flarum_mentions;
