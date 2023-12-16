d6db2057be8695fae09227c156d733befd1f81cb
corvofeng <corvofeng@gmail.com>
2023-12-16 23:13:35 +0800

    Update the flarum js.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- view/extensions/admin.js
+++ view/extensions/admin.js
@@ -1,8 +1,12 @@
 
-export * from './tags/js/admin';
+
+export * as flarum_tags from '../framework/extensions/tags/js/admin';
+export * from '../framework/extensions/markdown/js/admin';
+export * from '../framework/extensions/likes/js/admin';
 import * as flarum_mentions from './mentions/js/admin';
-export * from './likes/js/admin';
-export * from './custom-footer/js/admin';
+
+// export * from './custom-footer/js/admin';
 // export * from './auth-github/js/admin';
 // export * from './analytics/js/admin';
 flarum.extensions["flarum-mentions"] = flarum_mentions;
+flarum.extensions["flarum-tags"] = flarum_tags;
--- view/extensions/forum.js
+++ view/extensions/forum.js
@@ -1,14 +1,17 @@
 
-export * from './emoji/js/forum';
-export * from './recaptcha/js/forum';
+export * from '../framework/extensions/emoji/js/forum';
+import * as flarum_tags from '../framework/extensions/tags/js/forum';
+export * from '../framework/extensions/markdown/js/forum';
+import * as flarum_likes from '../framework/extensions/likes/js/forum';
 import * as flarum_mentions from './mentions/js/forum';
-export * from './tags/js/forum';
-export * from './markdown/js/forum';
+
+export * from './recaptcha/js/forum';
 // export * from './diff/js/forum';
-export * from './likes/js/forum';
+
 export * from './auth-github/js/forum';
 export * from './analytics/js/forum';
-export * from './custom-footer/js/forum';
+// export * from './custom-footer/js/forum';
 // export * from './flarum-pipetables/js/forum';
-
 flarum.extensions["flarum-mentions"] = flarum_mentions;
+flarum.extensions["tags"] = flarum_tags;
+flarum.extensions["likes"] = flarum_likes;
