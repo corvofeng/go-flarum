b629e75c833c013c8de5638f02a158846799e354
corvofeng <corvofeng@gmail.com>
2023-12-17 10:44:37 +0800

    Update admin support.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- view/extensions/admin.js
+++ view/extensions/admin.js
@@ -1,6 +1,6 @@
 
 
-export * as flarum_tags from '../framework/extensions/tags/js/admin';
+import * as flarum_tags from '../framework/extensions/tags/js/admin';
 export * from '../framework/extensions/markdown/js/admin';
 export * from '../framework/extensions/likes/js/admin';
 import * as flarum_mentions from './mentions/js/admin';
@@ -9,4 +9,4 @@ import * as flarum_mentions from './mentions/js/admin';
 // export * from './auth-github/js/admin';
 // export * from './analytics/js/admin';
 flarum.extensions["flarum-mentions"] = flarum_mentions;
-flarum.extensions["flarum-tags"] = flarum_tags;
+flarum.extensions["tags"] = flarum_tags;
