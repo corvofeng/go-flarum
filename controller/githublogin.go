87d0f5b0e662fd63713b67fbf61e63fe8a04efa8
corvofeng <corvofeng@gmail.com>
2024-07-01 12:28:04 +0800

    Update the github scope.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- controller/githublogin.go
+++ controller/githublogin.go
@@ -37,6 +37,7 @@ func GithubOauthHandler(w http.ResponseWriter, r *http.Request) {
 	h := ctx.h
 	gOauth := githubOauth(h.App.Cf.Site.GithubClientID, h.App.Cf.Site.GithubClientSecret)
 	gOauth.RedirectURL = h.App.Cf.Site.MainDomain + "/auth/github/callback"
+	gOauth.Scopes = []string{"user:email"}
 	url := gOauth.AuthCodeURL(oauthStateString)
 	http.Redirect(w, r, url, http.StatusFound)
 	return
